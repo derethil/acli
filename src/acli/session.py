8fd45f668eca0bb7a71486c5b703b0f023f24606
Jaren Glenn <jarenglenn@gmail.com>
2022-02-02 18:54:49 -0700

    header construction for posts

--- src/acli/session.py
+++ src/acli/session.py
@@ -15,6 +15,8 @@ class ASession:
         self._uri = None
         self._pos_id = None
 
+        self._login_res_url = None
+
         self._load_token()
 
     def login(self) -> bytes:
@@ -28,21 +30,35 @@ class ASession:
         if not res.url == f"{BASE_URL}/dashboard":
             raise ExecutionError("Incorrect username or password to Aggietime")
 
-        self._synchronize(res.content)
+        self._synchronize(res)
 
         return res.content
 
     def logged_in(self) -> bool:
         return self._token != None
 
-    def _synchronize(self, content: bytes) -> None:
-        parser = ParseHTML(content)
+    def post(self, url, data) -> Response:
+        data.update(
+            {
+                "SYNCHRONIZER_TOKEN": self._token,
+                "SYNCHRONIZER_URI": self._uri,
+                "posId": self._pos_id,
+            }
+        )
+
+        return self._session.post(
+            url=url, data=data, headers=self._construct_headers(data)
+        )
+
+    def _synchronize(self, res: Response) -> None:
+        parser = ParseHTML(res.content)
 
         self._token = parser.find_by_id("SYNCHRONIZER_TOKEN")
         self._uri = parser.find_by_id("SYNCHRONIZER_URI")
         self._pos_id = parser.find_by_id("posId")
+        self._login_res_url = res.request.url
 
-        set_token(self._token, self._uri, self._pos_id)
+        set_token(self._token, self._uri, self._pos_id, self._login_res_url)
 
     def _load_token(self) -> None:
         token = get_token()
@@ -59,3 +75,18 @@ class ASession:
         self._token = token["token"]
         self._uri = token["uri"]
         self._pos_id = token["pos_id"]
+        self._login_res_url = token["login_res_url"]
+
+    def _construct_headers(self, data: dict[str, str]) -> dict[str, str]:
+        return {
+            "HOST": "aggietime.usu.edu",
+            "CONTENT_TYPE": "application/x-www-form-urlencoded",
+            "ORIGIN": "https://aggietime.usu.edu",
+            "ACCEPT_ENCODING": "gzip, deflate, br",
+            "CONNECTION": "keep-alive",
+            "ACCEPT": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.55 Safari/537.36 Edg/98.0.1108.27",
+            "REFERER": f"{self._login_res_url}",
+            "ACCEPT_LANGUAGE": "en-us",
+            "CONTENT_LENGTH": f"{len(str(data))}",
+        }
