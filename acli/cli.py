af192cd65eaafd512f2197e887053899cbeb9b79
Jaren Glenn <jarenglenn@gmail.com>
2022-03-28 09:20:47 -0600

    moved src/acli into acli/ to fix poetry bug

---
+++ acli/cli.py
@@ -0,0 +1,114 @@
+#!/bin/python
+from functools import reduce
+
+from arc import CLI, State, callback, prompt, Context, logging
+from arc.present import Table
+from arc.present.table import Column
+from arc.present.data import justifications
+from arc.color import fg, colorize
+
+from .session import ASession
+from .login import login
+from .parser import ParseHTML
+from .config import BASE_URL
+from .utils import format_time
+
+
+cli = CLI(
+    name="acli",
+    # env="development",
+    state={"session": ASession()},
+)
+
+# Login command object
+cli.install_command(login)
+
+# Login decorator
+@callback.create()
+def login_required(args, ctx: Context):
+    session: ASession = ctx.state["session"]
+    session.login()
+
+    try:
+        yield
+    finally:
+        pass
+
+
+@login_required
+@cli.subcommand()
+def punch(state: State, *, comment: str = "", project_name: str = ""):
+    """Clock in or out of Aggietime
+    # Arguments
+    comment: Comment for your supervisor
+    project_name: Project name for current punch
+    """
+
+    session: ASession = state["session"]
+    to_status = ParseHTML(session.content).find_by_id("toStatus")
+
+    res = session.post(
+        url=f"{BASE_URL}/dashboard/clock/punch",
+        data={
+            "toStatus": to_status,
+            "comment": comment,
+            "projectName": project_name,
+        },
+    )
+
+    direction = "into" if to_status == "IN" else "out of"
+    if res.status_code == 200:
+        print(colorize(f"Successfully punched {direction} Aggietime!", fg.BRIGHT_CYAN))
+    else:
+        print(colorize("Something went wrong :(", fg.RED))
+
+
+@login_required
+@cli.subcommand()
+def status(state: State):
+    """Indicate whether you are clocked in or out of Aggietime"""
+
+    session: ASession = state["session"]
+    to_status = ParseHTML(session.content).find_by_id("toStatus")
+
+    if to_status == "OUT":
+        print(colorize("You're clocked in.", fg.BRIGHT_CYAN))
+    else:
+        print(colorize("You're clocked out.", fg.BRIGHT_CYAN))
+
+
+@login_required
+@cli.subcommand()
+def list(state: State):
+    """Select a shift and delete it from Aggietime's records."""
+
+    session: ASession = state["session"]
+    parser = ParseHTML(session.content)
+
+    parsed = parser.get_logged_hours()
+
+    rows = [
+        [
+            row["in_d"],
+            f'{row["in_t"]} - {row["out_t"]}',
+            format_time(float(row["hours"])),
+        ]
+        for row in parsed
+    ]
+
+    total = reduce(lambda x, y: x + y, [float(row["hours"]) for row in parsed])
+
+    def format_cell(content, column: Column, row_idx, column_idx):
+        return (
+            Table.formatter(
+                string=content,
+                width=10,
+                align=justifications[column["justify"]],
+                tcolor=f"{fg.WHITE}",
+            )
+            + " "
+        )
+
+    print(Table(["Date", "Time In/Out", "Hours"], rows=rows, format_cell=format_cell))
+
+    print(Table(["Total Hours"], rows=[[str(total)]], format_cell=format_cell))
---
+++ acli/parser.py
@@ -0,0 +1,39 @@
+from sys import base_prefix
+from typing import Dict, List
+from bs4 import BeautifulSoup
+from arc.types import Alias
+
+
+class ParseHTML:
+    def __init__(self, html_content: bytes) -> None:
+        self._soup = BeautifulSoup(html_content, "html5lib")
+
+    def get_logged_hours(self) -> List[dict[str, str]]:
+        return self._parse_hours()
+
+    def get_log_ids(self) -> List[int]:
+        id_inputs = self._soup.find_all("input", attrs={"data-shiftid": True})
+        return [int(item["data-shiftid"]) for item in id_inputs]
+
+    def find_by_id(self, to_search: str) -> str:
+        return self._soup.find(id=to_search).get("value")
+
+    def _parse_hours(self):
+        table = self._soup.find("table", id="pay-period").find("tbody")
+
+        def get_el(class_: str, *, el: str = "span") -> list[str]:
+            els = [el.getText().strip() for el in table.find_all(el, class_=class_)]
+            els.reverse()
+            return els
+
+        in_times = get_el("in-time")
+        out_times = get_el("out-time")
+        in_dates = get_el("in-date")
+        out_dates = get_el("out-date")
+
+        all_hours = list(filter(None, get_el("hours", el="td")))
+
+        return [
+            {"in_t": x[0], "out_t": x[1], "in_d": x[2], "out_d": x[3], "hours": x[4]}
+            for x in zip(in_times, out_times, in_dates, out_dates, all_hours)
+        ]
